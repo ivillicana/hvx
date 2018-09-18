6a943d7ce7d2eea21043ec5f85bd1d7f40c2bcfd
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-18 13:16:03 -0400

    import bootswatch, jquery, and popper.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,8 @@
+import 'bootswatch/dist/united/bootstrap.css'
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
