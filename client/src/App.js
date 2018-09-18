c7612c006c4a5b36131eed8206b097142a8d9552
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-18 13:15:23 -0400

    import NavBar and Footer components

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import NavBar from './containers/NavBar'
+import Footer from './containers/Footer'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='app'>
+        <NavBar />
+        <Footer />
       </div>
     );
   }
