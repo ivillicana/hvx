8754eb2859b19e9ab18d0ff9a84e273b2b68c5b3
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-18 14:12:05 -0400

    add Router to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,24 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Home from './components/Home';
+import LogIn from './components/LogIn';
+import NavBar from './containers/NavBar'
+import Footer from './containers/Footer'
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <React.Fragment>
+      <NavBar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={LogIn} />
+      <Footer />
+    </React.Fragment>
+  </Router>, 
+  document.getElementById('root')
+);
 registerServiceWorker();
