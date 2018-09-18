4c6007ee6c14674edeb1e257dc6384bdf8a01bf2
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-18 15:34:11 -0400

    update LogIn container

--- client/src/containers/LogIn.js
+++ client/src/containers/LogIn.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
+import LogInForm from '../components/LogInForm'
+import SignUpForm from '../components/SignUpForm'
 
 class LogIn extends Component {
   render() {
     return (
-      <div className='login'>
-        <p>This is LogIn component</p>
+      <div className="container">
+        <div className="row align-items-center">
+
+          <div className="col-md-5">
+            <div className="bs-component">
+              <LogInForm />
+            </div>
+          </div>
+
+          <div className="col-md-2">
+            <div className="bs-component">
+              <div className="text-center">
+                <strong>OR</strong>
+              </div>
+            </div>
+          </div>
+          
+          <div className="col-md-5">
+            <div className="bs-component">
+              <SignUpForm />
+            </div>
+          </div>
+        
+        </div>
       </div>
-    );
+    )
   }
 }
 
