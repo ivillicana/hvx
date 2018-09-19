943023e0fd6b6bd5cefa1d6ab2ccc73380131048
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-19 08:45:52 -0400

    change name of component to Activities

---
+++ client/src/containers/Activities.js
@@ -0,0 +1,10 @@
+import React, {Component}from 'react'
+
+class Activities extends Component {
+  render(){
+    return(
+      <div>This is Activities</div>
+    )
+  }
+}
+export default Activities
