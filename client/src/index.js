e3e27a4c96773a2683bcdf326e6510bdc794b4f9
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-19 08:41:58 -0400

    import Activities and EatAndDrink components

--- client/src/index.js
+++ client/src/index.js
@@ -6,28 +6,31 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
-import { createStore } from 'redux';
-import userReducer from './reducers/userReducer'
+import { Provider } from 'react-redux'
 
 import Home from './containers/Home';
 import LogIn from './containers/LogIn';
 import NavBar from './containers/NavBar'
 import Footer from './containers/Footer'
+import Activities from './containers/Activities'
+import EatAndDrink from './containers/EatAndDrink'
+import store from './store'
 
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(userReducer, 
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-
 ReactDOM.render(
-  <Router>
-    <React.Fragment>
-      <NavBar />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={LogIn} />
-      <Footer />
-    </React.Fragment>
-  </Router>, 
+  <Provider store={store}>
+    <Router>
+      <React.Fragment>
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={LogIn} />
+        <Route exaxt path="/activities" component={Activities}/>
+        <Route exaxt path="/eat-drink" component={EatAndDrink}/>
+        <Footer />
+      </React.Fragment>
+    </Router>
+  </Provider>, 
   document.getElementById('root')
 );
 registerServiceWorker();
