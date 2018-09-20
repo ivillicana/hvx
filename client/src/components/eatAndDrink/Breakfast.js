4af2faf49506544644924185e905f5a5cfcd525a
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-20 13:15:49 -0400

    render formatted restaurants in list group

--- client/src/components/eatAndDrink/Breakfast.js
+++ client/src/components/eatAndDrink/Breakfast.js
@@ -1,27 +1,27 @@
 import React, {Component} from 'react'
 
 class Breakfast extends Component {
+  formattedRestaurants () {
+    return this.props.restaurants.map(restaurant => {
+      const location = `${restaurant.location.address1}, ${restaurant.location.city}, ${restaurant.location.state} ${restaurant.location.zip_code}`
+
+      return <a href="#" className="list-group-item list-group-item-action flex-column align-items-start" role="tab">
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">{restaurant.name}</h5>
+            <small>{restaurant.location.city}</small>
+          </div>
+          <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+          <small>{location}</small>
+        </a>
+    })
+  }
 
   render(){
+
     return (
-      <div class="list-group">
-  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
-    <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1">List group item heading</h5>
-      <small>3 days ago</small>
-    </div>
-    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-    <small>Donec id elit non mi porta.</small>
-  </a>
-  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-    <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1">List group item heading</h5>
-      <small class="text-muted">3 days ago</small>
-    </div>
-    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-    <small class="text-muted">Donec id elit non mi porta.</small>
-  </a>
-</div>
+      <div className="list-group" id="list-tab" role="tablist">
+        {this.formattedRestaurants()}
+      </div>
     )
   }
 }
