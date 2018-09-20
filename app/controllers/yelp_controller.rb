94b6978f572ce102228598dee6c92a936c878f54
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-20 15:12:40 -0400

    add fetch_dinner action to yelp controller

--- app/controllers/yelp_controller.rb
+++ app/controllers/yelp_controller.rb
@@ -6,7 +6,7 @@ class YelpController < ApplicationController
   #change to environment variable
 
   def fetch_breakfast_and_brunch
-    resp = Faraday.get ('https://api.yelp.com/v3/businesses/search?location=newburgh&categories=breakfast_brunch&sort_by=rating&radius=40000') do |req|
+    resp = Faraday.get ('https://api.yelp.com/v3/businesses/search?categories=breakfast_brunch&sort_by=rating&location=newburgh&radius=40000&limit=10') do |req|
       req.headers['Authorization'] = 'Bearer GX_Sex34bJpD58wcxXMLluz0wksabbmzFQ2Tk1TGfPY3TmUJRsMMyW3gd7b0pLQoHs5X1ffG4HyCQXlVcCPtuIWez4AetJZsLDebPv4JJK0YPD2ou-litu4smbSjW3Yx'
     end
     body = JSON.parse(resp.body)
@@ -14,5 +14,13 @@ class YelpController < ApplicationController
     json_response(restaurants)
   end
 
+  def fetch_dinner
+    resp = Faraday.get ('https://api.yelp.com/v3/businesses/search?term=dinner&categories=restaurants&location=newburgh&radius=40000&limit=10') do |req|
+      req.headers['Authorization'] = 'Bearer GX_Sex34bJpD58wcxXMLluz0wksabbmzFQ2Tk1TGfPY3TmUJRsMMyW3gd7b0pLQoHs5X1ffG4HyCQXlVcCPtuIWez4AetJZsLDebPv4JJK0YPD2ou-litu4smbSjW3Yx'
+    end
+    body = JSON.parse(resp.body)
+    restaurants = body['businesses']
+    json_response(restaurants)
+  end
 
 end
