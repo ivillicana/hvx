3a2f73054877ec66a89e1095bba3f114fba63ff1
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-20 14:01:17 -0400

    update bootstrapo styling

--- client/src/components/eatAndDrink/Dinner.js
+++ client/src/components/eatAndDrink/Dinner.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react'
 
-class Breakfast extends Component {
+class Dinner extends Component {
   formattedRestaurants () {
     return this.props.restaurants.map(restaurant => {
       const location = `${restaurant.location.address1}, ${restaurant.location.city}, ${restaurant.location.state} ${restaurant.location.zip_code}`
 
-      return <a href="#" className="list-group-item list-group-item-action flex-column align-items-start" role="tab" key={restaurant.id}>
+      return <a href="" className="list-group-item list-group-item-action flex-column align-items-start" role="tab" key={restaurant.id}>
           <div className="d-flex w-100 justify-content-between">
             <h5 className="mb-2 font-weight-bold">{restaurant.name}</h5>
+            
           </div>
           <img style={{height: 150, width: '100%', maxWidth: 200, display: 'block'}} src={restaurant.image_url} alt="Card image"></img>
           <small>{location}</small>
@@ -26,4 +27,4 @@ class Breakfast extends Component {
 }
 
 
-export default Breakfast
+export default Dinner
