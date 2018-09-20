5b3e4ae9cf677a44d5956f4a6bbbc67145e4d4b0
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-20 15:09:48 -0400

    link restaurants to open in new tab

--- client/src/components/eatAndDrink/Breakfast.js
+++ client/src/components/eatAndDrink/Breakfast.js
@@ -5,11 +5,11 @@ class Breakfast extends Component {
     return this.props.restaurants.map(restaurant => {
       const location = `${restaurant.location.address1}, ${restaurant.location.city}, ${restaurant.location.state} ${restaurant.location.zip_code}`
 
-      return <a href="#" className="list-group-item list-group-item-action flex-column align-items-start" role="tab" key={restaurant.id}>
+      return <a href={restaurant.url} target="_blank" className="list-group-item list-group-item-action flex-column align-items-start" role="tab" key={restaurant.id}>
           <div className="d-flex w-100 justify-content-between">
             <h5 className="mb-2 font-weight-bold">{restaurant.name}</h5>
           </div>
-          <img style={{height: 150, width: '100%', maxWidth: 200, display: 'block'}} src={restaurant.image_url} alt="Card image"></img>
+          <img style={{height: 150, width: '100%', maxWidth: 200, display: 'block'}} src={restaurant.image_url} alt={restaurant.name}></img>
           <small>{location}</small>
         </a>
     })
