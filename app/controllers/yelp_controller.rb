84da609412f97196820400a16ebcc499ebe59ed9
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-20 22:26:53 -0400

    add Authorization header to requests

--- app/controllers/yelp_controller.rb
+++ app/controllers/yelp_controller.rb
@@ -7,7 +7,7 @@ class YelpController < ApplicationController
 
   def fetch_breakfast_and_brunch
     resp = Faraday.get ('https://api.yelp.com/v3/businesses/search?categories=breakfast_brunch&sort_by=rating&location=newburgh&radius=40000&limit=10') do |req|
-      req.headers['Authorization'] = 'Bearer GX_Sex34bJpD58wcxXMLluz0wksabbmzFQ2Tk1TGfPY3TmUJRsMMyW3gd7b0pLQoHs5X1ffG4HyCQXlVcCPtuIWez4AetJZsLDebPv4JJK0YPD2ou-litu4smbSjW3Yx'
+      req.headers['Authorization'] = "Bearer GX_Sex34bJpD58wcxXMLluz0wksabbmzFQ2Tk1TGfPY3TmUJRsMMyW3gd7b0pLQoHs5X1ffG4HyCQXlVcCPtuIWez4AetJZsLDebPv4JJK0YPD2ou-litu4smbSjW3Yx"
     end
     body = JSON.parse(resp.body)
     restaurants = body['businesses']
@@ -16,7 +16,16 @@ class YelpController < ApplicationController
 
   def fetch_dinner
     resp = Faraday.get ('https://api.yelp.com/v3/businesses/search?term=dinner&categories=restaurants&location=newburgh&radius=40000&limit=10') do |req|
-      req.headers['Authorization'] = 'Bearer GX_Sex34bJpD58wcxXMLluz0wksabbmzFQ2Tk1TGfPY3TmUJRsMMyW3gd7b0pLQoHs5X1ffG4HyCQXlVcCPtuIWez4AetJZsLDebPv4JJK0YPD2ou-litu4smbSjW3Yx'
+      req.headers['Authorization'] = "Bearer GX_Sex34bJpD58wcxXMLluz0wksabbmzFQ2Tk1TGfPY3TmUJRsMMyW3gd7b0pLQoHs5X1ffG4HyCQXlVcCPtuIWez4AetJZsLDebPv4JJK0YPD2ou-litu4smbSjW3Yx"
+    end
+    body = JSON.parse(resp.body)
+    restaurants = body['businesses']
+    json_response(restaurants)
+  end
+
+  def fetch_events
+    resp = Faraday.get ('https://api.yelp.com/v3/businesses/search?term=dinner&categories=restaurants&location=newburgh&radius=40000&limit=10') do |req|
+      req.headers['Authorization'] = "Bearer GX_Sex34bJpD58wcxXMLluz0wksabbmzFQ2Tk1TGfPY3TmUJRsMMyW3gd7b0pLQoHs5X1ffG4HyCQXlVcCPtuIWez4AetJZsLDebPv4JJK0YPD2ou-litu4smbSjW3Yx"
     end
     body = JSON.parse(resp.body)
     restaurants = body['businesses']
