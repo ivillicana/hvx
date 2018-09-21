4c00871e46256864cbbaa6a898b495d442d0eb64
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-20 22:25:25 -0400

    add navlink to logo

--- client/src/containers/NavBar.js
+++ client/src/containers/NavBar.js
@@ -9,7 +9,10 @@ class NavBar extends Component {
     return (
      <React.Fragment>
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary mb-5">
-        <a className="navbar-brand" href="#">Hudson Valley Xplorer</a>
+        <NavLink
+          to="/"
+          exact
+          ><span className="navbar-brand">Hudson Valley Xplorer</span></NavLink>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
