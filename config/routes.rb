ab0914bff042c5c695a51b8ce4db3d85dcdcaeef
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-21 13:39:08 -0400

    add activities routes to API

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
 
   get '/breakfast-and-brunch', to: "yelp#fetch_breakfast_and_brunch"
   get '/dinner', to: "yelp#fetch_dinner"
+
+  get '/activities/museums', to: "foursquare#fetch_museums"
+  get '/activities/nature', to: "foursquare#fetch_nature"
+  get '/activities/performing-arts', to: "foursquare#fetch_performing_arts"
 end
