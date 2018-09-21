9ee4ed11c3f45f025d42297b6f46e8cb569b3cbb
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-20 22:24:55 -0400

    add title and brief summary to Home container

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import Header from '../components/Header'
+import Background from '../images/hudson_valley.jpg';
+
 
 class Home extends Component {
   render() {
+    const title = "Hudson Valley Xplorer"
+    const briefSummary = "Welcome to Hudson Valley Xplorer - Live The Valley"
     return (
-      <div className='home'>
-        Home Component
+      <div className="container bg" >
+
+        <div className="row">
+          <div className="col-md-12">
+            <Header 
+            title={title}
+            briefSummary={briefSummary}/>          
+          </div>
+        </div>
+
       </div>
     );
   }
