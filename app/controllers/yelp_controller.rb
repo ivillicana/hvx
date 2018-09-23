7589b11fdf6f2e83f6fb5c4c3fe14558c8680b4a
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-23 11:22:28 -0400

    add #fetch_drinks and #fetch_restaurant actions

--- app/controllers/yelp_controller.rb
+++ app/controllers/yelp_controller.rb
@@ -23,6 +23,15 @@ class YelpController < ApplicationController
     json_response(restaurants)
   end
 
+  def fetch_drinks
+    resp = Faraday.get ('https://api.yelp.com/v3/businesses/search?categories=bars&location=newburgh&radius=40000&limit=10&sort_by=rating') do |req|
+      req.headers['Authorization'] = "Bearer GX_Sex34bJpD58wcxXMLluz0wksabbmzFQ2Tk1TGfPY3TmUJRsMMyW3gd7b0pLQoHs5X1ffG4HyCQXlVcCPtuIWez4AetJZsLDebPv4JJK0YPD2ou-litu4smbSjW3Yx"
+    end
+    body = JSON.parse(resp.body)
+    restaurants = body['businesses']
+    json_response(restaurants)
+  end
+
   def fetch_events
     resp = Faraday.get ('https://api.yelp.com/v3/businesses/search?term=dinner&categories=restaurants&location=newburgh&radius=40000&limit=10') do |req|
       req.headers['Authorization'] = "Bearer GX_Sex34bJpD58wcxXMLluz0wksabbmzFQ2Tk1TGfPY3TmUJRsMMyW3gd7b0pLQoHs5X1ffG4HyCQXlVcCPtuIWez4AetJZsLDebPv4JJK0YPD2ou-litu4smbSjW3Yx"
@@ -30,6 +39,14 @@ class YelpController < ApplicationController
     body = JSON.parse(resp.body)
     restaurants = body['businesses']
     json_response(restaurants)
+  end
+
+  def fetch_restaurant
+    resp = Faraday.get ("https://api.yelp.com/v3/businesses/#{params[:restaurantId]}") do |req|
+      req.headers['Authorization'] = "Bearer GX_Sex34bJpD58wcxXMLluz0wksabbmzFQ2Tk1TGfPY3TmUJRsMMyW3gd7b0pLQoHs5X1ffG4HyCQXlVcCPtuIWez4AetJZsLDebPv4JJK0YPD2ou-litu4smbSjW3Yx"
+    end
+    restaurant = JSON.parse(resp.body)
+    json_response(restaurant)
   end
 
 end
