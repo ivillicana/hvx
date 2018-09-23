a507cab0f7186760c9d6a094a63b4e14a04e1075
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-23 11:21:20 -0400

    add /drinks and /eat-drink/:restaurantId routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get '/breakfast-and-brunch', to: "yelp#fetch_breakfast_and_brunch"
   get '/dinner', to: "yelp#fetch_dinner"
+  get '/drinks', to: "yelp#fetch_drinks"
+  get '/eat-drink/:restaurantId', to: "yelp#fetch_restaurant"
 
   get '/activities/museums', to: "foursquare#fetch_museums"
   get '/activities/nature', to: "foursquare#fetch_nature"
