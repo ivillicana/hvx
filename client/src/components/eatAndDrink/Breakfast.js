bee59afc5bcba26bef595b9046ccbc3095e83fb7
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-23 11:29:09 -0400

    add links to RestaurantContainer

--- client/src/components/eatAndDrink/Breakfast.js
+++ client/src/components/eatAndDrink/Breakfast.js
@@ -1,17 +1,19 @@
 import React, {Component} from 'react'
+import { Link } from 'react-router-dom';
 
 class Breakfast extends Component {
   formattedRestaurants () {
     return this.props.restaurants.map(restaurant => {
-      const location = `${restaurant.location.address1}, ${restaurant.location.city}, ${restaurant.location.state} ${restaurant.location.zip_code}`
+      /*const location = `${restaurant.location.address1}, ${restaurant.location.city}, ${restaurant.location.state} ${restaurant.location.zip_code}`*/
 
-      return <a href={restaurant.url} target="_blank" className="list-group-item list-group-item-action flex-column align-items-start" role="tab" key={restaurant.id}>
+      return <Link key={restaurant.id} to={`eat-drink/${restaurant.id}`}><span className="list-group-item list-group-item-action flex-column align-items-start" role="tab" >
           <div className="d-flex w-100 justify-content-between">
             <h5 className="mb-2 font-weight-bold">{restaurant.name}</h5>
           </div>
-          <img style={{height: 150, width: '100%', maxWidth: 200, display: 'block'}} src={restaurant.image_url} alt={restaurant.name}></img>
-          <small>{location}</small>
-        </a>
+          {/*<img style={{height: 150, width: '100%', maxWidth: 200, display: 'block'}} src={restaurant.image_url} alt={restaurant.name}></img>
+          <small>{location}</small>*/}
+          </span>
+        </Link>
     })
   }
 
