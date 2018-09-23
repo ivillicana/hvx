c3f9a883fd0365a031cc57f03a4c9490c3c31527
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-23 11:24:12 -0400

    add FETCH_RESTAURANT and FETCH_DRINKS_RESTAURANTS

--- client/src/reducers/restaurantReducer.js
+++ client/src/reducers/restaurantReducer.js
@@ -1,8 +1,9 @@
 export default function restaurantReducer (
   state = { 
     breakfast: [],
-    lunch: [],
-    dinner: []
+    dinner: [],
+    drinks: [],
+    restaurant: {}
   },
   action
 ) {
@@ -13,6 +14,10 @@ export default function restaurantReducer (
       return {...state, breakfast: [...action.payload]}
     case 'FETCH_DINNER_RESTAURANTS':
       return {...state, dinner: [...action.payload]}
+    case 'FETCH_RESTAURANT':
+      return {...state, restaurant: {...action.payload}}
+    case 'FETCH_DRINKS_RESTAURANTS':
+      return {...state, drinks: [...action.payload]}
     default:
       return state;
   }
