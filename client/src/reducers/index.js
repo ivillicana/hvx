5c55fa570879de79b02eb6d93ca8090e70ce24f3
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-23 22:12:05 -0400

    remove UserReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import userReducer from './userReducer';
 import restaurantReducer from './restaurantReducer';
 import activitiesReducer from './activitiesReducer';
 
 export default combineReducers({
-  user: userReducer,
   restaurants: restaurantReducer,
   activities: activitiesReducer
 });
