1c1d714cbe69b5c5669e319d8ed55433c58a17cb
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-25 13:50:28 -0400

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+
   has_many :posts
   has_many :comments
 
-  has_secure_password
+  validates_presence_of :name, :email, :password_digest, :location
 end
