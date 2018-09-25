73f3fdab8006dd088d95d58155ea199bf6a22f2b
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-25 18:59:47 -0400

    create postsActions

---
+++ client/src/actions/postActions.js
@@ -0,0 +1,37 @@
+export const fetchPosts = () => dispatch =>{
+  const jwtToken = localStorage.getItem('jwtToken')
+  fetch('/posts', {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${jwtToken}`
+    }
+  })
+  .then(resp => resp.json())
+  .then(postsData => dispatch({
+    type: 'FETCH_POSTS',
+    payload: postsData
+  }))
+  
+}
+
+export const createNewPost = (postData) => dispatch => {
+  const jwtToken = localStorage.getItem('jwtToken')
+  fetch('/posts', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${jwtToken}`
+    },
+    body: {
+      title: postData.title,
+      content: postData.content
+    }
+  })
+    .then(resp => resp.json())
+    .then(post => dispatch({
+      type: "CREATE_POST",
+      payload: post
+    }))
+}
