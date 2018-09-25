2d51b24755a52961a02b94b1f21bd5e408bb54b4
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-25 17:57:03 -0400

    remove withRouter

--- client/src/containers/NavBar.js
+++ client/src/containers/NavBar.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import { Link, NavLink } from 'react-router-dom';
 import {connect} from 'react-redux'
-import {withRouter} from 'react-router-dom'
+
 import {logOutUser} from '../actions/userActions'
 
 class NavBar extends Component {
