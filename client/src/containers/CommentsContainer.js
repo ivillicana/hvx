96e88115cfd02fcbc37c9171ef0e0bc1543423d4
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-26 13:05:30 -0400

    add key to CommentForm and remove Connect()

--- client/src/containers/CommentsContainer.js
+++ client/src/containers/CommentsContainer.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import CommentShow from '../components/neighborhood/CommentShow'
 import CommentForm from '../components/neighborhood/CommentForm';
+import '../components/neighborhood/neighborhood.css'
 
 class CommentsContainer extends Component {
 
@@ -9,13 +10,13 @@ class CommentsContainer extends Component {
     return(
       <div className="row">
           <div className="col-md-12">
-            <h4>Comments</h4>
+            <h4 className="mb-3 text-center">Comments</h4>
             {comments && 
               comments.map(comment =>{
                 return <CommentShow key={comment.id} comment={comment}/>
               })
             }
-            <CommentForm />
+            <CommentForm key={this.props.postId} postId={this.props.postId} createNewComment={this.props.createNewComment} />
           </div>
         </div>
     )
