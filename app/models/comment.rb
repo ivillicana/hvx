5873c5e69ff424bb45bc9c69c391495e25052e94
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-26 10:31:13 -0400

    add #user_name and #user_location

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,12 @@ class Comment < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :content
+
+  def user_name
+    self.user ? self.user.name : nil
+  end
+
+  def user_location
+    self.user ? self.user.location : nil
+  end
 end
