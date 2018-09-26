e196695640ee6c20e0dd0d6b26c5afb11a8a043b
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-26 13:04:04 -0400

    add componentDidMount()

--- client/src/containers/PostShow.js
+++ client/src/containers/PostShow.js
@@ -1,33 +1,50 @@
 import React, {Component} from 'react'
 import CommentsContainer from './CommentsContainer'
+import {connect} from 'react-redux'
+import {createNewComment, fetchPosts, fetchPost} from '../actions/postActions'
+import '../components/neighborhood/neighborhood.css'
 
 class PostShow extends Component {
 
+  componentDidMount(){
+    this.props.fetchPosts();
+    this.props.fetchPost(this.props.match.params.id);
+  }
+
   render(){
-    const foundPost = this.props.location.state.post
+    const foundPost = this.props.post
     return(
       <React.Fragment>
-        <div className="container bg-primary rounded text-white pt-3 mb-4">
-          <div className="row">
-            <div className="col-md-12">
-              <h2>{foundPost.title}</h2>
-              <p><small>Posted by: {foundPost.user.name}</small></p>
+        {foundPost &&
+          <React.Fragment>
+          <div className="container rounded text-white pt-3 mb-4 post-container">
+            <div className="row">
+              <div className="col-md-12">
+                <h2>{foundPost.title}</h2>
+                <p><small>—{foundPost.user.name} ({foundPost.user.location})</small></p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <p>{foundPost.content}</p>
+              </div>
             </div>
           </div>
-          <div className="row">
-            <div className="col-md-12">
-              <p>{foundPost.content}</p>
-            </div>
-          </div>
-        </div>
 
-        <div className="container border border-primary rounded p-5">
-            {foundPost.comments && 
-              <CommentsContainer comments={foundPost.comments}/>
-            }
-        </div>
+          <div className="container rounded p-5 comment-container">
+              {foundPost.comments && 
+                <CommentsContainer postId={foundPost.id} comments={foundPost.comments} createNewComment={this.props.createNewComment}/>
+              }
+          </div>
+          </React.Fragment>
+        }
       </React.Fragment>
     )
   }
 }
-export default PostShow
+
+const mapStateToProps = state => ({
+  post: state.post
+})
+
+export default connect(mapStateToProps, {createNewComment, fetchPosts, fetchPost})(PostShow)
