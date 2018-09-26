bf31ed7ecd04ba27736ac8f8de5cc7344ee3f48d
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-26 13:30:22 -0400

    remove Routes

--- client/src/containers/EatAndDrink.js
+++ client/src/containers/EatAndDrink.js
@@ -1,12 +1,10 @@
 import React, { Component }from "react";
 import {connect} from 'react-redux'
-import {Route} from 'react-router-dom'
 
 import Header from '../components/Header'
 import Breakfast from '../components/eatAndDrink/Breakfast'
 import Dinner from '../components/eatAndDrink/Dinner'
 import Drinks from '../components/eatAndDrink/Drinks'
-import RestaurantContainer from '../components/eatAndDrink/RestaurantContainer'
 
 import {fetchBreakfastItems} from '../actions/eatAndDrinkActions'
 import {fetchDinnerItems} from '../actions/eatAndDrinkActions'
@@ -23,7 +21,8 @@ class EatAndDrink extends Component {
     const title = "Eat & Drink"
     const briefSummary = "Looking for a place to eat or enjoy a drink while in the Hudson Valley?"
 
-    const EatAndDrinkContainer = (<div className="container">
+    return(
+      <div className="container">
         <div className="row">
           <div className="col-md-12">
             <Header 
@@ -50,14 +49,6 @@ class EatAndDrink extends Component {
         </div>
         
       </div>
-    )
-
-    return(
-      <React.Fragment>
-        <Route exact path={this.props.match.url} render={() => EatAndDrinkContainer} />
-        <Route exact path={`${this.props.match.url}/:restaurantId`} render={routerProps =>          <RestaurantContainer {...routerProps} />}/>
-      </React.Fragment>
-      
     )
   }
 }
