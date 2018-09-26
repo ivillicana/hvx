90c3f7dc34de1a3a255d46e44e67b665a143ebba
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-26 10:30:46 -0400

    add user_name and user_location to comment serializer attributes

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :content, :post_id, :user_id
+  attributes :id, :content, :post_id, :user_id, :user_name, :user_location
   belongs_to :post 
   belongs_to :user
 end
