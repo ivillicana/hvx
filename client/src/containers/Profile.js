3f30bdbfc6da233b0a2e141c7f84fd9a44a09a2b
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-02-21 18:43:38 -0500

    add state to Profile component

--- client/src/containers/Profile.js
+++ client/src/containers/Profile.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
 class Profile extends Component {
+  state = {
+    user: null
+  }
+
+  // componentDidMount() {
+  //   this.getUserData()
+  // }
+
+  // getUserData = () => {
+  //   const jwtToken = localStorage.getItem('jwtToken')
+  //   fetch('/user', {
+  //     headers: {
+  //       Accept: 'application/json',
+  //       'Content-Type': 'application/json',
+  //       Authorization: `Bearer ${jwtToken}`
+  //     }
+  //   })
+  //   .then(resp => resp.json())
+  //   .then(userInfo => {
+  //     this.setState({user: userInfo})
+  //   })
+  // }
+
   render() {
+    let user = this.state.user
     return (
       <div>
-        Profile Page
+        {/* {user &&
+        <div>
+          <p>{user.name}</p>
+          <p>{user.email}</p>
+          <p>{user.location}</p>
+        </div>
+        } */}
       </div>
     );
   }
